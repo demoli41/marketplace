c8ad09fe2dac952bc16eab9c3c516885e51f073c
Bohdan <werewtyu23@gmail.com>
2025-04-30 11:17:38 +0300

    User ui setup and header component

---
+++ apps/user-ui/src/configs/constants.tsx
@@ -0,0 +1,22 @@
+export const navItems:NavItemsTypes[]=[
+    {
+        title: 'Головна',
+        href: '/'
+    },
+    {
+        title: 'Товари',
+        href: '/products'
+    },
+    {
+        title: 'Магазини',
+        href: '/shops'
+    },
+    {
+        title: 'Пропозиції',
+        href: '/offers'
+    },
+    {
+        title: 'Стати продавцем',
+        href: '/become-seller'
+    },
+]
